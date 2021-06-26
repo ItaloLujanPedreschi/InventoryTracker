9cc138917442a72511204e4f44d4995969ffec54
italo <italo.lujanpedreschi@gmail.com>
2021-06-25 20:52:49 -0700

    Routes complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     root "items#index"
 
-    get "/items", to: "items#index"
+    resources :users, only: [:show, :create]
+    resource :session, only: [:create, :destroy]
+    resources :businesses, only: [:index, :show, :create, :update, :destroy]
+    resources :items, only: [:index, :show, :create, :update, :destroy]
+    resources :units, only: [:index, :show, :create, :update, :destroy]
 end
