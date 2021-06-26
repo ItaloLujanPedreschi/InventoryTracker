5be58f925b4c4363e0c16fd39ffe408114a72f49
italo <italo.lujanpedreschi@gmail.com>
2021-06-26 13:24:25 -0700

    Edit added to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
 
     resources :users, only: [:new, :show, :create]
     resource :session, only: [:new, :create, :destroy]
-    resources :businesses, only: [:new, :show, :create, :update, :destroy] do
+    resources :businesses, only: [:new, :show, :create, :edit, :update, :destroy] do
         resources :items, only: [:new]
     end
-    resources :items, only: [:show, :create, :update, :destroy] do
+    resources :items, only: [:show, :create, :edit, :update, :destroy] do
         resources :units, only: [:new]
     end
-    resources :units, only: [:show, :create, :update, :destroy]
+    resources :units, only: [:show, :create, :edit, :update, :destroy]
 end
