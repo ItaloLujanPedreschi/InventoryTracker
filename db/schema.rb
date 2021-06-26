54b17b8e9b60e8db728e9022bf021fef87093c89
italo <italo.lujanpedreschi@gmail.com>
2021-06-25 20:25:35 -0700

    Users migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_26_024844) do
+ActiveRecord::Schema.define(version: 2021_06_26_031842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2021_06_26_024844) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["business_id"], name: "index_items_on_business_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "session_token", null: false
+    t.string "password_digest", null: false
+    t.string "name", null: false
+    t.string "email", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 
 end
