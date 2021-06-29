19f901c7b6ca7eaca980cee18790c829102c935b
italo <italo.lujanpedreschi@gmail.com>
2021-06-29 13:45:20 -0700

    Item show routes removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    root "sessions#new"
+    root "users#new"
 
     resources :users, only: [:new, :show, :create]
     resource :session, only: [:new, :create, :destroy]
     resources :businesses, only: [:new, :show, :create, :edit, :update, :destroy] do
         resources :items, only: [:new]
     end
-    resources :items, only: [:show, :create, :edit, :update, :destroy]
+    resources :items, only: [:create, :edit, :update, :destroy]
 end
